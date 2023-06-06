cb7053f5fd4fe49e46a2400ba5bafdc8cd2dcf06
giorgio <giorgio.programmer@gmail.com>
2023-06-06 15:50:34 +0200

    starting to experimenting with backend

---
+++ src/main/java/com/example/application/endpoints/helloreact/Todo.java
@@ -0,0 +1,6 @@
+package com.example.application.endpoints.helloreact;
+
+public class Todo {
+    private String task;
+    private boolean done;
+}
---
+++ src/main/java/com/example/application/endpoints/helloreact/TodoEndpoint.java
@@ -0,0 +1,13 @@
+package com.example.application.endpoints.helloreact;
+
+import org.checkerframework.checker.units.qual.A;
+
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+
+import dev.hilla.Endpoint;
+
+@Endpoint
+@AnonymousAllowed
+public class TodoEndpoint {
+    
+}
